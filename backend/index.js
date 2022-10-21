3407ff3238ac6ae372239be102bacd59039b193b
Zsombor Szántó <zsombor.szanto@adnovum.hu>
2022-10-21 21:10:21 +0200

    Add pipeline and prereqs

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,8 @@ const app = express()
 
 app.use(cors());
 
+app.use(express.static('../frontend/dist'))
+
 app.get('/about-info', function (req, res) {
     res.status(200).json({
         name:'Attila'
