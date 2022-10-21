f2b807d42f542df4884c8c93c62f99f1d9149a06
Medgyesi Attila <attila.medgyesi@goodgrind.tech>
2022-10-22 00:10:50 +0200

    Add marker sample logic

--- frontend/src/components/GoogleMapLoader.vue
+++ frontend/src/components/GoogleMapLoader.vue
@@ -41,6 +41,28 @@ export default {
     initializeMap() {
       const mapContainer = this.$refs.googleMap
       this.map = new this.google.maps.Map(mapContainer, mapConfig())
+      this.addSampleMarker()
+    },
+    addSampleMarker() {
+      const tempCost = 23122
+      const mySVG = `
+        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 180 44" style="enable-background:new 0 0 180 44;" xml:space="preserve">
+          <style type="text/css">
+            .st0{fill:#0097C7;}
+            .st1{font-family:'AvertaDemoPECuttedDemo-Regular';}
+            .st2{font-size:30px;}
+          </style>
+          <path class="st0" d="M171,44H9c-5,0-9-4-9-9V9c0-5,4-9,9-9h162c5,0,9,4,9,9v26C180,40,176,44,171,44z"/>
+          <text transform="matrix(1 0 0 1 21.4214 32.9702)" class="st1 st2">${tempCost.toLocaleString()} Ft</text>
+        </svg>
+      `
+
+      new this.google.maps.Marker({
+        position: { lat: 47.4848873, lng: 19.076491 },
+        map: this.map,
+        icon:{ url: 'data:image/svg+xml;charset=UTF-8,' + encodeURIComponent(mySVG), scaledSize: new this.google.maps.Size(100, 100) }
+      });
+
     }
   }
 }
