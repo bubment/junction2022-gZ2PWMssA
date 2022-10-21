c648d201d7a15404c735e02afce37463971e1f82
Zsombor Szántó <zsombor.szanto@adnovum.hu>
2022-10-21 23:53:28 +0200

    Add missing frontend dep

--- frontend/src/services/backend-service.js
+++ frontend/src/services/backend-service.js
@@ -2,12 +2,17 @@ import axios from "axios";
 
 const HOST = 'http://localhost:'
 const PORT = '3000'
+let URL = `${HOST}${PORT}`
+
+if (import.meta.env.PROD) {
+    URL = eval("window.location.origin")
+}
 
 export default {
     getAbout: async function () {
-        return axios.get(`${HOST}${PORT}/about-info`)
+        return axios.get(`${URL}/about-info`)
     },
     getGoogleMapsApiKey: async function () {
-        return axios.get(`${HOST}${PORT}/google-maps-api-key`)
+        return axios.get(`${URL}/google-maps-api-key`)
     },
 }
