d2ef2938e9b2feddcca4f7f67f3a9ae1d1a1e7ca
Bálint Nagy <grandbalint@gmail.com>
2022-10-22 16:41:02 +0200

    add asset folder to static

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,7 @@ app.use(cors());
 app.use(express.json())
 
 app.use(express.static('../frontend/dist'))
+app.use(express.static('assets'))
 
 app.get('/about-info', function (req, res) {
     res.status(200).json({
